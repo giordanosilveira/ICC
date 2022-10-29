8b1a4d2ed890bec9e94049c6c1a60160d3b4a376
giordanosilveira <giordanovraau0204@gmail.com>
2022-10-28 21:22:59 -0300

    Acho que terminei os exercícios

--- exercicios_s1/ex2.c
+++ exercicios_s1/ex2.c
@@ -3,6 +3,17 @@
 #include <float.h>
 #include <math.h>
 
+
+/*B)
+        Para números não normalizados, o expoente vai para 0, porém a mantissa é um número diferente
+    de 0. Além disso, para números muito pequenos não a distinção de um para outro. Eles atingem um número
+    mínimo e permanecem neste número, ou seja, precisão dos números diminui bruscamente.
+        Para números normalizados a precisão aumenta. Há muito mais números entre 1.0e-38 e 2.0e-38 do que 1.0e-45 e 2.0e-45 que são número
+    não normalizados. Ou seja, quando os números são normalizados e possível representar muito mais números
+    entre dois floats perto do zero do que quando os números são denormalizados. 
+
+*/
+
 typedef union {
 
     int32_t i;
@@ -22,22 +33,53 @@ void printFloat_t(Float_t num) {
 
 void inicializar_float_t(Float_t *a_float){
     a_float[0].f = 0.0;
-    a_float[1].f = 0.2;
-    a_float[2].f = 1.0;
-    a_float[3].f = 1.5;
-    a_float[4].f = 1.75;
-    a_float[5].f = 1.99999988;
-    a_float[6].f = 2.0;
-    a_float[7].f = 16777215;
-     a_float[8].f = 1.410129846*pow(10,-45);
+    a_float[1].f = 1.410129846*pow(10,-45);
+    a_float[2].f = 1.4101298465444444*pow(10,-45);
+    a_float[3].f = 1.41012*pow(10,-45);
+    a_float[4].f = 3.41012*pow(10,-45);
+    a_float[5].f = 3.0*pow(10,-45);
+    a_float[6].f = 5.2*pow(10,-45);
+    a_float[7].f = 10.41012*pow(10,-45);
+    a_float[8].f = 100.41012*pow(10,-45);
+    a_float[9].f = 1.41012984*pow(10,-44);
+    a_float[10].f = 4.1012984*pow(10,-44);
+    a_float[11].f = 10.41012984*pow(10,-44);
+    a_float[12].f = 20.41012984*pow(10,-44);
+    a_float[13].f = 100.41012984*pow(10,-43);
+    //a_float[2].f = 1.410129847*pow(10,-45);
+    a_float[14].f = 1.410129850*pow(10,-38);
+    a_float[15].f = 1.41012985012345*pow(10,-38);
+    a_float[16].f = 1.4101*pow(10,-38);
+    a_float[17].f = 2.4101*pow(10,-38);
+    a_float[18].f = 3.4101*pow(10,-38);
+    a_float[19].f = 10000.4101*pow(10,-38);
+    a_float[20].f = 100.4101*pow(10,-38);
+    a_float[21].f = 10000.4101*pow(10,-38);
+    a_float[22].f = 20.4101*pow(10,-38);
+    a_float[23].f = 50.4101*pow(10,-38);
+    a_float[24].f = 38.4101*pow(10,-38);
+    a_float[25].f = 45.4101*pow(10,-38);
+    a_float[26].f = 100000.4101*pow(10,-37);
+    a_float[27].f = 1.4101*pow(10,-37);
+    a_float[28].f = 0.2;
+    a_float[29].f = 1.0;
+    a_float[30].f = 1.5;
+    a_float[31].f = 1.75;
+    a_float[32].f = 1.99999988;
+    a_float[33].f = 2.0;
+    a_float[34].f = 16777215;
+    a_float[35].f = (3.40282347*pow(10, 38));
+    a_float[36].f = (1.410129846*pow(10, 10))/0;
+    a_float[37].f = (-1.410129846*pow(10, 10))/0;
+    a_float[38].f = 0.0/0;
 } 
 
 int main() {
 
-    Float_t array[12];
+    Float_t array[40];
 
     inicializar_float_t(array);
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 40; i++) {
         printFloat_t(array[i]);
     }
     printf("\nEpsilon: %1.15f\n", FLT_EPSILON);
--- exercicios_s1/ex3.c
+++ exercicios_s1/ex3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct erro_float_t {
   
@@ -12,25 +13,58 @@ void soma(erro_float_t A, erro_float_t B){
 
     erro_float_t soma_minima;
     soma_minima.num = (A.num - A.erro) + (B.num - B.erro);
-    printf("O menor valor de A é %1.2f e o de B é %1.2f\n", A.num - A.erro, B.num - B.erro);
-    printf("Portanto, A + B = %1.2f + %1.2f = %1.2f\n", A.num - A.erro, B.num - B.erro, soma_minima.num);
+    printf("O menor valor de A é %1.6f e o de B é %1.6f\n", A.num - A.erro, B.num - B.erro);
+    printf("Portanto, A + B = %1.6f + %1.6f = %1.6f\n", A.num - A.erro, B.num - B.erro, soma_minima.num);
 
     erro_float_t soma_maxima;
     soma_maxima.num = (A.num + A.erro) + (B.num + B.erro);
-    printf("O máximo valor de A é %1.2f e o de B é %1.2f\n", A.num + A.erro, B.num + B.erro);
-    printf("Portanto, A + B = %1.2f + %1.2f = %1.2f\n", A.num + A.erro, B.num + B.erro, soma_maxima.num);
+    printf("O máximo valor de A é %1.6f e o de B é %1.6f\n", A.num + A.erro, B.num + B.erro);
+    printf("Portanto, A + B = %1.6f + %1.6f = %1.6f\n", A.num + A.erro, B.num + B.erro, soma_maxima.num);
 
-    printf("Assim, %1.2f <= A + B <= %1.2f -> X + Y = %1.2f +- %1.2f\n", soma_minima.num, soma_maxima.num, soma_maxima.num - (soma_maxima.num - soma_minima.num)/2,(soma_maxima.num - soma_minima.num)/2);
+    printf("Assim, %1.6f <= A + B <= %1.6f -> X + Y = %1.6f +- %1.6f\n", soma_minima.num, soma_maxima.num, soma_maxima.num - (soma_maxima.num - soma_minima.num)/2,(soma_maxima.num - soma_minima.num)/2);
 }
-// void produto(erro_float_t A, erro_float_t B){
+void produto(erro_float_t A, erro_float_t B){
 
-// }
-// void potencia(erro_float_t A, erro_float_t B){
+    erro_float_t produto_minima;
+    produto_minima.num = (A.num - A.erro) * (B.num - B.erro);
+    printf("O menor valor de A é %1.6f e o de B é %1.6f\n", A.num - A.erro, B.num - B.erro);
+    printf("Portanto, A * B = %1.6f * %1.6f = %1.6f\n", A.num - A.erro, B.num - B.erro, produto_minima.num);
 
-// }
-// void divisao(erro_float_t A, erro_float_t B){
+    erro_float_t produto_maxima;
+    produto_maxima.num = (A.num + A.erro) * (B.num + B.erro);
+    printf("O máximo valor de A é %1.6f e o de B é %1.6f\n", A.num + A.erro, B.num + B.erro);
+    printf("Portanto, A * B = %1.6f * %1.6f = %1.6f\n", A.num + A.erro, B.num + B.erro, produto_maxima.num);
 
-// }
+    printf("Assim, %1.6f <= A * B <= %1.6f -> X * Y = %1.6f +- %1.6f\n", produto_minima.num, produto_maxima.num, produto_maxima.num - (produto_maxima.num - produto_minima.num)/2,(produto_maxima.num - produto_minima.num)/2);
+}
+void potencia(erro_float_t A, erro_float_t B){
+    erro_float_t potencia_minima;
+    potencia_minima.num = pow((A.num - A.erro), (B.num - B.erro));
+    printf("O menor valor de A é %1.6f e o de B é %1.6f\n", A.num - A.erro, B.num - B.erro);
+    printf("Portanto, A^B = %1.6f^%1.6f = %1.6f\n", A.num - A.erro, B.num - B.erro, potencia_minima.num);
+
+    erro_float_t potencia_maxima;
+    potencia_maxima.num = pow((A.num + A.erro), (B.num + B.erro));
+    printf("O máximo valor de A é %1.6f e o de B é %1.6f\n", A.num + A.erro, B.num + B.erro);
+    printf("Portanto, A^B = %1.6f^%1.6f = %1.6f\n", A.num + A.erro, B.num + B.erro, potencia_maxima.num);
+
+    printf("Assim, %1.6f <= A^B <= %1.6f -> X^Y = %1.6f +- %1.6f\n", potencia_minima.num, potencia_maxima.num, potencia_maxima.num - (potencia_maxima.num - potencia_minima.num)/2,(potencia_maxima.num - potencia_minima.num)/2);
+}
+void divisao(erro_float_t A, erro_float_t B){
+    
+    erro_float_t divisao_minima;
+    divisao_minima.num = (A.num - A.erro)/(B.num - B.erro);
+    printf("O menor valor de A é %1.6f e o de B é %1.6f\n", A.num - A.erro, B.num - B.erro);
+    printf("Portanto, A/B = %1.6f/%1.6f = %1.6f\n", A.num - A.erro, B.num - B.erro, divisao_minima.num);
+
+    erro_float_t divisao_maxima;
+    divisao_maxima.num = (A.num + A.erro)/(B.num + B.erro);
+    printf("O máximo valor de A é %1.6f e o de B é %1.6f\n", A.num + A.erro, B.num + B.erro);
+    printf("Portanto, A/B = %1.6f/%1.6f = %1.6f\n", A.num + A.erro, B.num + B.erro, divisao_maxima.num);
+
+    printf("Assim, %1.6f <= A/B <= %1.6f -> X/Y = %1.6f +- %1.6f\n", divisao_minima.num, divisao_maxima.num, divisao_maxima.num - (divisao_maxima.num - divisao_minima.num)/2,(divisao_maxima.num - divisao_minima.num)/2);
+
+}
 
 int main () {
 
@@ -38,12 +72,20 @@ int main () {
     printf("Entre com o primeiro número e o erro:\n>");
     scanf("%f %f", &A.num, &A.erro);
 
-    printf("Entre com o primeiro número e o erro:\n>");
+    printf("Entre com o segundo número e o erro:\n>");
     scanf("%f %f", &B.num, &B.erro);
 
+    printf("Soma:\n");
     soma(A, B);
-    // produto(A, B);
-    // potencia(A, B);
-    // divisao(A, B);
+    printf("\n");
+    printf("Produto:\n");
+    produto(A, B);
+    printf("\n");
+    printf("Potência:\n");
+    potencia(A, B);
+    printf("\n");
+    printf("Divisão:\n");
+    divisao(A, B);
+    printf("\n");
 
 }
