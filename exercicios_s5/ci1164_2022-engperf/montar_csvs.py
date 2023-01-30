d2dd1887bac1788d67f207a821b3381096f1d94d
Giba pingo <giordanovraau0204@gmail.com>
2023-01-30 18:53:42 -0300

    scripts

--- exercicios_s5/ci1164_2022-engperf/montar_csvs.py
+++ exercicios_s5/ci1164_2022-engperf/montar_csvs.py
@@ -1,22 +1,37 @@
 
-saida_csv = f"L2CACHE.csv"
-
+grupos = [("FLOPS_DP","\nDP MFLOP/s"), ("L2CACHE","\nL2 miss ratio"), ("L3","\nL3 bandwidth [MBytes/s]")]
 tamanhos = [64, 100, 128, 1024, 2000, 2048]
+
+for grupo in grupos:
+    lista = list()
+    lista_tempo = list()
+    for tam in tamanhos:
+        with open(f"{tam}_{grupo[0]}.csv", "r") as arq:
+            f = arq.read()
+            f = f.split(',')
+            lista.append([f[i + 1] for i, elemento in enumerate(f) if elemento == grupo[1]])
+
+        lista[len(lista) - 1].insert(0, f"{tam}")
+
+    with open(f"{grupo[0]}.csv", 'w') as arq2:
+            arq2.write("Tamanho,t_matvet,t_matmat\n")
+            for m in lista:
+                arq2.write(f"{','.join(m)}\n")
+
+
 lista = list()
 for tam in tamanhos:
-    with open(f"{tam}_L2CACHE.csv", "r") as arq:
+    with open(f"{tam}_{grupo[0]}.csv", "r") as arq:
         f = arq.read()
         f = f.split(',')
-        lista.append([f[i + 1] for i, elemento in enumerate(f) if elemento == '\nRuntime (RDTSC) [s]'])
+        lista.append([f[i + 1] for i, elemento in enumerate(f) if elemento == "\nRuntime (RDTSC) [s]"])
+
     lista[len(lista) - 1].insert(0, f"{tam}")
 
-with open(saida_csv, 'w') as arq2:
-        arq2.write("Tamanho,t_matvet,t_matmat\n")
-        for m in lista:
-            arq2.write(f"{','.join(m)}\n")
-
-
-    
+    with open(f"TIME.csv", 'w') as arq2:
+            arq2.write("Tamanho,t_matvet,t_matmat\n")
+            for m in lista:
+                arq2.write(f"{','.join(m)}\n") 
 
 
     
--- exercicios_s5/ci1164_2022-engperf/test.py
+++ exercicios_s5/ci1164_2022-engperf/test.py
@@ -4,6 +4,42 @@ os.system("make")
 
 params_likwid = ["FLOPS_DP", "L2CACHE", "L3"]
 tam = [64, 100, 128, 1024, 2000, 2048]
-for k in tam:
-    for i in params_likwid:
-        os.system(f"likwid-perfctr -O -C 3 -g {i} -m ./matmult -n {k} > {k}_{i}.csv")
+
+grupos = [("FLOPS_DP","\nDP MFLOP/s"), ("L2CACHE","\nL2 miss ratio"), ("L3","\nL3 bandwidth [MBytes/s]")]
+tamanhos = [64, 100, 128, 1024, 2000, 2048]
+
+for tam in tamanhos:
+    for grupo in grupos:
+        os.system(f"likwid-perfctr -O -C 3 -g {grupo[0]} -m ./matmult -n {tam} > {tam}_{grupo[0]}.csv")
+
+
+for grupo in grupos:
+    lista = list()
+    lista_tempo = list()
+    for tam in tamanhos:
+        with open(f"{tam}_{grupo[0]}.csv", "r") as arq:
+            f = arq.read()
+            f = f.split(',')
+            lista.append([f[i + 1] for i, elemento in enumerate(f) if elemento == grupo[1]])
+
+        lista[len(lista) - 1].insert(0, f"{tam}")
+
+    with open(f"{grupo[0]}.csv", 'w') as arq2:
+            arq2.write("Tamanho,t_matvet,t_matmat\n")
+            for m in lista:
+                arq2.write(f"{','.join(m)}\n")
+
+
+lista = list()
+for tam in tamanhos:
+    with open(f"{tam}_{grupo[0]}.csv", "r") as arq:
+        f = arq.read()
+        f = f.split(',')
+        lista.append([f[i + 1] for i, elemento in enumerate(f) if elemento == "\nRuntime (RDTSC) [s]"])
+
+    lista[len(lista) - 1].insert(0, f"{tam}")
+
+    with open(f"TIME.csv", 'w') as arq2:
+            arq2.write("Tamanho,t_matvet,t_matmat\n")
+            for m in lista:
+                arq2.write(f"{','.join(m)}\n")
